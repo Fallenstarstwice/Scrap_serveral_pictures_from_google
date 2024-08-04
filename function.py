6b830437248b2aaf2235bc3e5625d9b2c62dd9a7
Fallenstarstwice <168614206+Fallenstarstwice@users.noreply.github.com>
2024-08-04 13:48:39 +0800

    1.0.1

---
+++ function.py
@@ -0,0 +1,35 @@
+import requests,logging,random
+
+
+#use to download images
+def download_img(robot_image,contents):
+    num=1
+    for img in robot_image:
+        src=img['src']
+        
+        if(f"{''.join(  [src[0],src[1],src[2],src[3] ] )}"=="http"):
+            logging.info(src)
+            img=requests.get(src).content
+            with open(f"./image/{contents}/{num}.png","wb") as out_file:
+                    out_file.write(img)
+            endstring=f"Downloading {num}/{len(robot_image)}".rjust(5)
+            print(endstring,end="")
+            print('\b'*len(endstring),end="",flush=True)
+            num+=1
+
+
+# return user-agent
+def return_ua():
+     ua_list=[
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.1",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.3",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.3",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/25.0 Chrome/121.0.0.0 Safari/537.3",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.3",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.3",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.3",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Unique/100.7.6266.6",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 OPR/112.0.0."
+]
+     return({"User-Agent":{random.choice(ua_list)}})
