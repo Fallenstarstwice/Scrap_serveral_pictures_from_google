18d7f2d4a33a88ef1ff7f5592e44cdff65a36748
Fallenstarstwice <168614206+Fallenstarstwice@users.noreply.github.com>
2024-08-04 15:06:27 +0800

    1.1.0

--- function.py
+++ function.py
@@ -32,4 +32,4 @@ def return_ua():
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.",
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 OPR/112.0.0."
 ]
-     return({"User-Agent":{random.choice(ua_list)}})
+     return({"User-Agent":f"{random.choice(ua_list)}"})
